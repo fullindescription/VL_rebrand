d0cca631d304bc56fa5c6dfb462b20c0933fc5a1
Alexander <blue_chill@icloud.com>
2024-10-15 12:23:47 +1000

    jwt авторизация

--- VL_rebrand/users/models.py
+++ VL_rebrand/users/models.py
@@ -1,3 +1,21 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class User(AbstractUser):
+    email = models.EmailField(unique=True)
+
+    # Добавьте related_name для полей groups и user_permissions
+    groups = models.ManyToManyField(
+        'auth.Group',
+        related_name='custom_user_set',  # Уникальное имя обратной связи
+        blank=True,
+        help_text='The groups this user belongs to.',
+        verbose_name='groups'
+    )
+    user_permissions = models.ManyToManyField(
+        'auth.Permission',
+        related_name='custom_user_permissions_set',  # Уникальное имя обратной связи
+        blank=True,
+        help_text='Specific permissions for this user.',
+        verbose_name='user permissions'
+    )
