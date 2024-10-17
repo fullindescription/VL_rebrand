9ce93d8f09e817590c24673b75acc34ac0029b22
SIVOKHINsa <serega_9598@mail.ru>
2024-10-17 17:15:42 +0300

    Добавлены API для получения events

--- VL_rebrand/events/urls.py
+++ VL_rebrand/events/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from .views import get_film_by_name, get_films_for_day, add_or_update_cart_item, get_cart, remove_cart_item
+from .views import get_film_by_name, get_films_for_day, add_or_update_cart_item, get_cart, remove_cart_item, get_event_by_name, get_events_for_day
 
 urlpatterns = [
     path('getfilmbyname/', get_film_by_name, name='get_film_by_name'),
-   path('getfilmsforday/', get_films_for_day, name='get_films_for_day'),
-    path('add_or_update_cartitem/', add_or_update_cart_item, name='add_or_update_cart_item'),
+    path('getfilmsforday/', get_films_for_day, name='get_films_for_day'),
+    path('geteventbyname/', get_event_by_name, name='get_event_by_name'),
+    path('geteventsforday/', get_events_for_day, name='get_events_for_day'),
+    path('cart/add_or_update_cartitem/', add_or_update_cart_item, name='add_or_update_cart_item'),
     path('cart/', get_cart, name='get-cart'),
     path('cart/item_remove/<int:item_id>/', remove_cart_item, name='remove-cart-item'),
 ]
--- VL_rebrand/events/views.py
+++ VL_rebrand/events/views.py
@@ -1,13 +1,19 @@
 from django.shortcuts import get_object_or_404
-from .models import Movie, MovieSession
-from .serializers import MovieSerializer, MovieSessionSerializer
+from .models import Movie, MovieSession, Event, Cart, CartItem
+from .serializers import MovieSerializer, MovieSessionSerializer, EventSerializer, CartItemSerializer
 from datetime import datetime
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework import status
-from .models import Cart, CartItem
-from .serializers import CartItemSerializer
+
+from datetime import datetime, timedelta
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Event
+from .serializers import EventSerializer
+
 
 @api_view(['GET'])
 def get_film_by_name(request):
@@ -80,6 +86,7 @@ def get_films_for_day(request):
 
     return Response(response_data, status=status.HTTP_200_OK)
 
+
 @api_view(['POST'])
 @permission_classes([IsAuthenticated])
 def add_or_update_cart_item(request):
@@ -87,12 +94,9 @@ def add_or_update_cart_item(request):
         user = request.user
         cart, _ = Cart.objects.get_or_create(user=user)
 
-        # Создаем изменяемую копию request.data
         data = request.data.copy()
-        # Добавляем cart в данные запроса
         data['cart'] = cart.id
 
-        # Передаем измененные данные в сериализатор
         serializer = CartItemSerializer(data=data, context={'cart': cart})
         if serializer.is_valid():
             serializer.save()
@@ -101,7 +105,8 @@ def add_or_update_cart_item(request):
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     except Exception as e:
-        return Response({"error": "An unexpected error occurred: " + str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        return Response({"error": "An unexpected error occurred: " + str(e)},
+                        status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 
 @api_view(['GET'])
@@ -114,14 +119,15 @@ def get_cart(request):
         if not cart:
             return Response({"message": "Cart is empty."}, status=status.HTTP_200_OK)
 
-        # Получаем все элементы корзины для текущего пользователя
         cart_items = CartItem.objects.filter(cart=cart)
         serializer = CartItemSerializer(cart_items, many=True)
 
         return Response({"cart_items": serializer.data}, status=status.HTTP_200_OK)
 
     except Exception as e:
-        return Response({"error": "An unexpected error occurred: " + str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        return Response({"error": "An unexpected error occurred: " + str(e)},
+                        status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
 
 @api_view(['DELETE'])
 @permission_classes([IsAuthenticated])
@@ -133,11 +139,62 @@ def remove_cart_item(request, item_id):
         if not cart:
             return Response({"error": "Cart not found."}, status=status.HTTP_404_NOT_FOUND)
 
-        # Проверяем, что элемент корзины принадлежит этому пользователю
         cart_item = get_object_or_404(CartItem, cart=cart, id=item_id)
         cart_item.delete()
 
         return Response({"message": "Item removed from cart."}, status=status.HTTP_204_NO_CONTENT)
 
     except Exception as e:
-        return Response({"error": "An unexpected error occurred: " + str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        return Response({"error": "An unexpected error occurred: " + str(e)},
+                        status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+
+@api_view(['GET'])
+def get_event_by_name(request):
+    name = request.GET.get('name', None)
+
+    if not name:
+        return Response({"error": "Please provide an event name"}, status=status.HTTP_400_BAD_REQUEST)
+
+    try:
+        event = Event.objects.get(name__iexact=name)
+    except Event.DoesNotExist:
+        return Response({"error": "Event not found"}, status=status.HTTP_404_NOT_FOUND)
+
+    event_serializer = EventSerializer(event)
+
+    event_id = event.id
+
+    return Response({
+        "event": event_serializer.data
+    }, status=status.HTTP_200_OK)
+
+
+@api_view(['GET'])
+def get_events_for_day(request):
+    date = request.GET.get('date', None)
+
+    if not date:
+        return Response({"error": "Please provide a date in the format YYYY-MM-DD"},
+                        status=status.HTTP_400_BAD_REQUEST)
+
+    try:
+        date_obj = datetime.strptime(date, '%Y-%m-%d')
+    except ValueError:
+        return Response({"error": "Invalid date format. Please use YYYY-MM-DD."},
+                        status=status.HTTP_400_BAD_REQUEST)
+
+    # Устанавливаем начало и конец дня
+    start_of_day = date_obj
+    end_of_day = date_obj + timedelta(days=1)
+
+    # Фильтрация событий по диапазону дат
+    events = Event.objects.filter(date__gte=start_of_day, date__lt=end_of_day)
+
+    if not events.exists():
+        return Response({"message": "No events found for this date."},
+                        status=status.HTTP_404_NOT_FOUND)
+
+    # Сериализуем и возвращаем список событий
+    event_serializer = EventSerializer(events, many=True)
+    return Response(event_serializer.data, status=status.HTTP_200_OK)
